76d0f34310ad64de2f4a494f3398c0f150292a44
Dmitry Kovtun <mr_write@mail.ru>
2022-01-18 11:06:59 +0300

    SQRService-version-2

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,16 +1,13 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public static void main(String[] args) {
-
-    }
-
-    public static int calculateSQR (int downRande, int upRange) {
+    public int calculateSQR(int downRande, int upRange) {
         int counter = 0;
-        for (int i = 10; i < 99; i++) {
-            if (i * i >= downRande && i * i <= upRange) counter++;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= downRande && i * i <= upRange) {
+                counter++;
+            }
         }
         return counter;
     }
-
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -11,7 +11,7 @@ class SQRServiceTest {
     @ParameterizedTest
     @CsvSource(value = {"simple test, 200, 300, 3",
             "lower range limit below first square, 90, 300, 8",
-            "extensive test, 1000, 20000, 67",
+            "extensive test, 1000, 20000, 68",
             "first square does not fall within the border, 80, 90, 0",
             "lower range and upper range swapped, 200, 90, 0"
     })
